e56bdca65ce2d8dd87ac1e27072bf9827457eca5
Reilly Keele <reillykeele@gmail.com>
2022-03-31 22:48:29 -0600

    A5

--- sockets.py
+++ sockets.py
@@ -13,11 +13,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+from http import client
 import flask
 from flask import Flask, request, redirect
 from flask_sockets import Sockets
 import gevent
-from gevent import queue
+from gevent.queue import Queue
+from geventwebsocket import WebSocketError
+from geventwebsocket.websocket import WebSocket
 import time
 import json
 import os
@@ -59,10 +62,13 @@ class World:
     def world(self):
         return self.space
 
-myWorld = World()        
+myWorld = World()
+clients = []
 
 def set_listener( entity, data ):
     ''' do something with the update ! '''
+    # I don't know what I'm supposed to do with this 
+    return
 
 myWorld.add_set_listener( set_listener )
         
@@ -71,18 +77,59 @@ def hello():
     '''Return something coherent here.. perhaps redirect to /static/index.html '''
     return redirect("/static/index.html")
 
-def read_ws(ws,client):
+def read_ws(ws: WebSocket, client: Queue):
     '''A greenlet function that reads from the websocket and updates the world'''
     # XXX: TODO IMPLEMENT ME
-    return None
+    while not ws.closed:
+        try:        
+            message = ws.receive()    
+            if message == None: break
+
+            # we got a message! update the entity
+            obj = json.loads(message)
+            for key in obj:
+                myWorld.set(key, obj[key])
+
+            # broadcast to all connected clients 
+            for c in clients:
+                c.put(message)
+
+        except WebSocketError:
+            # socket is dead
+            break
+
 
 @sockets.route('/subscribe')
-def subscribe_socket(ws):
+def subscribe_socket(ws: WebSocket):
     '''Fufill the websocket URL of /subscribe, every update notify the
        websocket and read updates from the websocket '''
     # XXX: TODO IMPLEMENT ME
-    return None
+    print('press like and subscribe')
+    
+    # inspired by Nicholas Pufal on Stack Overflow https://stackoverflow.com/a/27072972
+    # based on greenlet gist https://sdiehl.github.io/gevent-tutorial/
+    # create a queue for our greenlet, and add it to our connect clients
+    # spawn a greelet process to continuously read from the socker and add
+    # to the queues of our connect clients! 
+    client = Queue()
+    clients.append(client)
+    greenlet = gevent.spawn(read_ws, ws, client)
 
+    while not ws.closed:
+        try:
+            # get message from our queue!
+            data = client.get()
+
+            # echo message to our client 
+            ws.send(data)
+        except WebSocketError:
+            # socket is dead
+            break
+
+    # socket closed or died, remove from clients and end its greenlet process
+    print('socket clsosed')
+    clients.remove(client)
+    gevent.kill(greenlet)
 
 # I give this to you, this is how you get the raw body/data portion of a post in flask
 # this should come with flask but whatever, it's not my project.
